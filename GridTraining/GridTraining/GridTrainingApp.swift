a9643edaf45e336bfe91343efd6ca7f565da69ed
岡優志 <yu.j.0513@gmail.com>
2022-02-05 20:09:47 +0900

    -

---
+++ GridTraining/GridTraining/GridTrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridTrainingApp.swift
+//  GridTraining
+//
+//  Created by oka yuuji on 2022/02/05.
+//
+
+import SwiftUI
+
+@main
+struct GridTrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
