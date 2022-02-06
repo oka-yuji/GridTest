98b407422e3c236229895ae81422b468740c536d
岡優志 <yu.j.0513@gmail.com>
2022-02-06 21:06:22 +0900

    -

---
+++ GridTraining/GridTraining/SwiftUIView2.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView2.swift
+//  GridTraining
+//
+//  Created by oka yuuji on 2022/02/06.
+//
+
+import SwiftUI
+
+struct SwiftUIView2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIView2_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView2()
+    }
+}
