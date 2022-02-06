902c029b57a5bcce704e229bd989e316bd952c14
岡優志 <yu.j.0513@gmail.com>
2022-02-06 17:05:39 +0900

    -

--- GridTraining/GridTraining/DocumentCodeTest.swift
+++ GridTraining/GridTraining/DocumentCodeTest.swift
@@ -8,22 +8,16 @@
 import SwiftUI
 
 struct DocumentCodeTest: View {
+    let data = Array(1...1000)
+    let layout = [GridItem(.adaptive(minimum: 30))]
     var columns: [GridItem] = Array(repeating: .init(.flexible(minimum: 10, maximum: 300)), count: 10)
     var body: some View {
         ScrollView {
-            LazyVGrid(columns: columns) {
-                ForEach((0...79), id: \.self) {
-                    let codepoint = $0 + 0x1f600
-                    //let codepointString = String(format: "%02X", codepoint)
-                    let emoji = String(Character(UnicodeScalar(codepoint)!))
-                    HStack {
-                        Spacer()
-                    //Text("\(codepointString)")
-                    Text("\(emoji)")
-                        Spacer()
-                    }
+            LazyVGrid(columns: layout) {
+                ForEach(data, id: \.self) { item in
+                Text("\(item)")
                 }
-            }.font(.system(size: 10))
+            }
         }
     }
 }
