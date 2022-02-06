2144ee72ef1294c0953808539526591535e8297a
岡優志 <yu.j.0513@gmail.com>
2022-02-06 20:51:37 +0900

    -

--- GridTraining/GridTraining/DocumentCodeTest.swift
+++ GridTraining/GridTraining/DocumentCodeTest.swift
@@ -8,25 +8,25 @@
 import SwiftUI
 
 struct DocumentCodeTest: View {
-    let data = Array(1...1000)
-    let layout = [GridItem(.adaptive(minimum: 100))]
-    var body: some View {
-        ScrollView {
-            LazyVGrid(columns: layout) {
-                ForEach(data, id: \.self) { item in
-                    VStack {
-                        Text("\(item)")
-                          Rectangle()
-                            .frame(height: 5)
-                    }
+let data = Array(1...1000)
+let layout = [GridItem(.adaptive(minimum: 50))]
+var body: some View {
+    ScrollView {
+        LazyVGrid(columns: layout) {
+            ForEach(data, id: \.self) { item in
+                VStack {
+                    Text("\(item)")
+                      Rectangle()
+                        .frame(height: 5)
                 }
             }
         }
     }
 }
+}
 
 struct DocumentCodeTest_Previews: PreviewProvider {
-    static var previews: some View {
-        DocumentCodeTest()
-    }
+static var previews: some View {
+    DocumentCodeTest()
+}
 }
