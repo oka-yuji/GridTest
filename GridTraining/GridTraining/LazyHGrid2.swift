a9c99b6e24d4c842fd4e51eb60139911aac9965e
岡優志 <yu.j.0513@gmail.com>
2022-02-10 19:35:22 +0900

    -

--- GridTraining/GridTraining/LazyHGrid2.swift
+++ GridTraining/GridTraining/LazyHGrid2.swift
@@ -11,22 +11,20 @@ struct LazyHGrid2: View {
     let data = Array(1...100).map { "Index\($0)" }
     let layout = [
         GridItem(.flexible(maximum: 80)),
-        GridItem(.flexible(maximum: 100), spacing: 100),
+        GridItem(.flexible(maximum: 100)),
         GridItem(.flexible(maximum: 150))
     ]
     var rows: [GridItem] =
     Array(repeating: .init(.fixed(20)), count: 2)
     var body: some View {
         ScrollView(.horizontal) {
-            LazyHGrid(rows: rows, alignment: .top) {
-                ForEach((0...79), id: \.self) {
-                    let codepoint = $0 + 0x1f600
-                    let codepointString = String(format: "%02X", codepoint)
-                    Text("\(codepointString)")
-                        .font(.footnote)
-                    let emoji = String(Character(UnicodeScalar(codepoint)!))
-                    Text("\(emoji)")
-                        .font(.largeTitle)
+            LazyHGrid(rows: layout, spacing: 10) {
+                ForEach(data, id: \.self) { item in
+                    LazyVStack {
+                        Text("\(item)")
+                        Rectangle()
+                            .frame(height: 5)
+                    }
                 }
             }
         }
