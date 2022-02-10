032bf8c66d8ec4e0fa2207699c9f5ad254f39aa2
岡優志 <yu.j.0513@gmail.com>
2022-02-10 19:37:12 +0900

    -

--- GridTraining/GridTraining/LazyHGrid2.swift
+++ GridTraining/GridTraining/LazyHGrid2.swift
@@ -8,31 +8,29 @@
 import SwiftUI
 
 struct LazyHGrid2: View {
-    let data = Array(1...100).map { "Index\($0)" }
-    let layout = [
-        GridItem(.flexible(maximum: 80)),
-        GridItem(.flexible(maximum: 100)),
-        GridItem(.flexible(maximum: 150))
-    ]
-    var rows: [GridItem] =
-    Array(repeating: .init(.fixed(20)), count: 2)
-    var body: some View {
-        ScrollView(.horizontal) {
-            LazyHGrid(rows: layout, spacing: 10) {
-                ForEach(data, id: \.self) { item in
-                    LazyVStack {
-                        Text("\(item)")
-                        Rectangle()
-                            .frame(height: 5)
-                    }
+let data = Array(1...100).map { "Index\($0)" }
+let layout = [
+    GridItem(.flexible(maximum: 80)),
+    GridItem(.flexible(maximum: 100)),
+    GridItem(.flexible(maximum: 150))
+]
+var body: some View {
+    ScrollView(.horizontal) {
+        LazyHGrid(rows: layout, spacing: 10) {
+            ForEach(data, id: \.self) { item in
+                LazyVStack {
+                    Text("\(item)")
+                    Rectangle()
+                        .frame(height: 5)
                 }
             }
         }
     }
 }
+}
 
 struct LazyHGrid2_Previews: PreviewProvider {
-    static var previews: some View {
-        LazyHGrid2()
-    }
+static var previews: some View {
+    LazyHGrid2()
+}
 }
